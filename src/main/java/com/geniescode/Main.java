ca07ce5b6feecc015f6a5f72e27073e907e9bdaa
genie <litebohojamesmoalosi@gmail.com>
2023-05-16 12:01:31 +0200

    add changes in main class

--- src/main/java/com/geniescode/Main.java
+++ src/main/java/com/geniescode/Main.java
@@ -2,10 +2,8 @@ package com.geniescode;
 
 import com.geniescode.frontend.LogInFrame;
 
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         new LogInFrame().setVisible(true);
     }
 }
